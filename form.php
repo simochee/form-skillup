858e55e4ca7d31e0b3e8fe5fabe2a236bbbf84be
Ryoya Tamura <simochee@gmail.com>
2017-10-24 15:33:40 +0900

    フォーム部分をつくった

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <form action="result.php" method="GET"></form>
+
+  <form action="result.php" method="GET">
+    <input name="name">
+    <input name="age">
+    <input type="submit" value="送信">
+  </form>
+</body>
+</html>
